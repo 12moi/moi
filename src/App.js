ea15ae89a3b7d78bf5757b0614736cc8b6735c5c
devmoi <moishadrack.com>
2023-01-12 21:25:05 +0300

    changes the icon

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import { About } from './components/About/About';
 import { Contact } from './components/Contact/Contact';
+import { Footer } from './components/Footer/Footer';
 import { Home } from './components/Home/Home';
 import { Portfolio } from './components/Portfolio/Portfolio';
 
@@ -13,6 +14,7 @@ function App() {
      <About />
      <Portfolio />
      <Contact />
+     <Footer />
     </div>
   );
 }
