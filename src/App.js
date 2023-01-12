96567923bbbeba71946ca9ff07e21b33dfdd0520
devmoi <moishadrack.com>
2023-01-12 20:39:25 +0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
 import './App.css';
+import { About } from './components/About/About';
+import { Contact } from './components/Contact/Contact';
+import { Home } from './components/Home/Home';
+import { Portfolio } from './components/Portfolio/Portfolio';
+
 
 function App() {
   return (
     <div className="App">
-      
-    <h1>Hello</h1>
+      <Home />
+     <About />
+     <Portfolio />
+     <Contact />
     </div>
   );
 }
