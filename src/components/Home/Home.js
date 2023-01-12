cc10ffc6d729691bc080a784e24dc8580a8afe43
devmoi <moishadrack.com>
2023-01-12 22:07:07 +0300

    add styles to Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,11 +3,13 @@ import './home.css'
 
 export const Home = () => {
   return (
-    <div>
+    <div className='home'>
+        <div className='container'>
         <h5>Hello, I'm</h5>
       <h1>Moi Shadrack</h1>
       <h5 className='text-light' id="dev">Front-end Developer</h5>
       <a href='#contact' className='scroll__downd'>Scroll Downd</a>
+        </div>
     </div>
   )
 }
