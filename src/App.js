942493639e1de24b7cda48645787976b8e628b96
devmoi <moishadrack.com>
2023-01-13 15:03:59 +0300

    add updates

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 
 import './App.css';
-import { About } from './components/About/About';
-import { Contact } from './components/Contact/Contact';
-import { Footer } from './components/Footer/Footer';
-import { Home } from './components/Home/Home';
-import { Portfolio } from './components/Portfolio/Portfolio';
-
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Home />
-     <About />
-     <Portfolio />
-     <Contact />
-     <Footer />
+      <Navbar />
     </div>
   );
 }
