35df6c86085b77fc072f9b68af6e434a0ccd5d77
devmoi <moishadrack.com>
2023-01-13 20:24:28 +0300

    add data file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 
+
 function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
--- src/components/About.js
+++ src/components/About.js
@@ -7,13 +7,13 @@ export default function About() {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Moi Shadrack.
-            <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
+            <br className="hidden lg:inline-block" />Building amazing
+            web applications.
           </h1>
           <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+          I am a tech-driven individual, responsible, and committed software engineer,
+           with an a-get-it-done, on-time and high-quality 
+           product spirit to implement and deliver an amazing web applications.
           </p>
           <div className="flex justify-center">
             <a
