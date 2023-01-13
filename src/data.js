de924eb29aef1fc9a7ac1048fe0029cc2ca6192e
devmoi <moishadrack.com>
2023-01-13 20:33:35 +0300

    add images

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export const projects = [
     {
-      title: "React Reserve",
+      title: "Quiz App",
       subtitle: "MERN Stack",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
