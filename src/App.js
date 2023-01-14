34e1b9fcb5969abad9ca283dc62791554d3f4ea7
devmoi <moishadrack.com>
2023-01-14 09:35:54 +0300

    add imports

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
+import Projects from './components/Projects';
 import Skills from './components/Skills';
 
 
@@ -10,6 +11,7 @@ function App() {
     <main className="text-gray-400 bg-gray-900 body-font">
     <Navbar />
     <About />
+    <Projects />
     <Skills />
   </main>
 );
