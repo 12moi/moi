c70e4ce89506c22cfb66060ebcd189fbd1c4bc44
devmoi <moishadrack.com>
2023-01-14 13:47:19 +0300

    add changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import About from './components/About';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
@@ -15,6 +16,7 @@ function App() {
     <Projects />
     <Skills />
     <Contact />
+    <Footer />
   </main>
 );
   
---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import '../App.css'
+import {BsInstagram} from 'react-icons/bs'
+import {GrTwitter} from 'react-icons/gr'
+
+export default function Footer() {
+  return (
+    
+    <footer>
+    <a href='' className='footer__logo'>MOI</a>
+    <ul className='pernalinks'>
+      <li><a href='#'>Home</a></li>
+      <li><a href='#about'>About</a></li>
+      <li><a href='#experience'>Experience</a></li>
+      <li><a href='#portfolio'>Portfolio</a></li>
+      <li><a href='#contact'>Contact</a></li>
+    </ul>
+    <div className='footer__social'>
+      
+      <a href='https://twitter.com'><GrTwitter/></a>
+      <a href='https://instagram.com'><BsInstagram/></a>
+      
+    </div>
+    <div className='footer__copyright'>
+      <small>
+        &copy; MOI. All rights reserved
+      </small>
+    </div>
+  </footer>
+  );
+}
