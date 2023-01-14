c54fc30a52b3b1836399ca17759a93f99e1521a8
devmoi <moishadrack.com>
2023-01-14 14:12:18 +0300

    add updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,10 +9,10 @@ export default function Footer() {
     <footer>
     <a href='' className='footer__logo'>MOI</a>
     <ul className='pernalinks'>
-      <li><a href='#'>Home</a></li>
+      {/* <li><a href='#'>Home</a></li> */}
       <li><a href='#about'>About</a></li>
-      <li><a href='#experience'>Experience</a></li>
-      <li><a href='#portfolio'>Portfolio</a></li>
+      <li><a href='#skills'>Skills</a></li>
+      <li><a href='#projects'>Projects</a></li>
       <li><a href='#contact'>Contact</a></li>
     </ul>
     <div className='footer__social'>
